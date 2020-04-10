74c2cb61d36c74d6cd19a29e7ae8a477f375edf5
Vaclav Zelenkevic <vaclav.zelenkevic@vtmc.lt>
2020-04-10 03:14:41 +0300

    Kazkoks pakeitimas ...

--- src/main/java/multithreading/Scheduler.java
+++ src/main/java/multithreading/Scheduler.java
@@ -7,12 +7,13 @@ public class Scheduler {
     private final Thread thread;
 
     private volatile boolean destroyed = false;
+    private int counter = 0;
 
     public Scheduler(TimeUnit timeUnit, long time, Runnable runnable) {
         this.thread = new Thread(() -> {
             while (!destroyed) {
                 runnable.run();
-
+                System.out.println("I am running: " + counter++ + " s");
                 try {
                     timeUnit.sleep(time);
                 } catch (InterruptedException e) {}
